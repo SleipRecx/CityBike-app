affbcab22122cdf7270f28784e64a49a0c3be3ef
Markus Andresen <markua@stud.ntnu.no>
2016-05-10 12:34:08 +0200

    all bike locations is now visible

--- Bysykkel/ViewController.swift
+++ Bysykkel/ViewController.swift
@@ -10,43 +10,50 @@ import UIKit
 import MapKit
 import CoreLocation
 
-
-
 class ViewController: UIViewController {
 
     @IBOutlet weak var map: MKMapView!
 
     let locationManager = CLLocationManager()
-    
-    var longitude: Double = 0
-    var latitude: Double = 0
-    var availableBikes: Int = 0
-    var availableSlots: Int = 0
-    var adress: String = ""
-    var online: Bool = false
-
+    var places: [BikePlace] = []
+    var currentPlace: [BikePlace] = []
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude )
+        currentPlace[0].location.coordinate.latitude
+        let location = CLLocationCoordinate2D(latitude: currentPlace[0].location.coordinate.latitude, longitude: currentPlace[0].location.coordinate.longitude )
         let annotation = MKPointAnnotation()
         annotation.coordinate = location
-        if online{
+        if currentPlace[0].online{
             annotation.title = "Åpen"
         }
         else{
             annotation.title = "Stengt"
         }
         
-        annotation.subtitle = "Sykler: " + String(availableBikes) + " Låser: " + String(availableSlots)
+        annotation.subtitle = "Sykler: " + String(currentPlace[0].availableBikes) + " Låser: " + String(currentPlace[0].availableSlots)
         map.addAnnotation(annotation)
         let span = MKCoordinateSpan(latitudeDelta: 0.007, longitudeDelta: 0.007)
         let region = MKCoordinateRegion(center: location, span:  span)
         map.setRegion(region, animated: true)
         map.selectAnnotation(map.annotations[0], animated: true)
         self.map.showsUserLocation = true
-        self.navigationItem.title = adress;
+        self.navigationItem.title = currentPlace[0].adress;
+        populateMap()
+    }
+    
+    
+    func populateMap(){
+        for place in places{
+            let location = place.location
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location.coordinate
+            annotation.title = place.adress
+            annotation.subtitle = "Sykler: " + String(place.availableBikes) + " Låser: " + String(place.availableSlots)
+            map.addAnnotation(annotation)
+        }
+        
     }
 
 
@@ -56,7 +63,6 @@ class ViewController: UIViewController {
     }
     
     
-    @IBOutlet weak var dd: UILabel!
 
    
 
