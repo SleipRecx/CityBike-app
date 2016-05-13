95cdcbd5b95e1466bc0dd19bb792279f203f6fa9
Markus Andresen <markua@stud.ntnu.no>
2016-05-13 11:59:45 +0200

    new file

---
+++ Bysykkel/UINavigationBarTaller (1).swift
@@ -0,0 +1,21 @@
+//
+//  UINavigationBarTaller.swift
+//  Bysykkel
+//
+//  Created by Markus Andresen on 11/05/16.
+//  Copyright © 2016 Markus Andresen. All rights reserved.
+//
+
+import UIKit
+
+class UINavigationBarTaller: UINavigationBar {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
