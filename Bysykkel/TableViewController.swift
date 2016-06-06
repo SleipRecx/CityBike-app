7e72ac7a608f6f1effec5e2f45ec1310792d37ff
Markus Andresen <markua@stud.ntnu.no>
2016-06-06 15:49:15 +0200

    Removed override for didSelectRowAtIndexPath

--- Bysykkel/TableViewController.swift
+++ Bysykkel/TableViewController.swift
@@ -226,11 +226,7 @@ class TableViewController: UITableViewController, UISearchResultsUpdating, CLLoc
         self.refreshController.endRefreshing()
     }
     
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let cell: CustomCell = self.tableView.cellForRowAtIndexPath(indexPath) as! CustomCell
-        cell.img.backgroundColor = getCellColor(getCurrentTableViewArray()[indexPath.row])
-     
-    }
+   
     
     override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
         let unFav = UITableViewRowAction(style: .Normal, title: "Fjern"){(
